665a484b30773daef49aaf730705d58019111005
ckgibb61 <giblinck@gmail.com>
2018-03-18 11:38:24 -0600

    Add files via upload

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,13 +7,14 @@ $(document).ready(function(){
      for (var i = 0; i < topics.length; i++) {
         console.log(topics[i]);
         var Btn = $("<button>").text(topics[i]);
-        // Btn.attr();
-        // Btn.css({padding: 8px, color: blue});
         $("#buttons").append(Btn);
     };
-    var character = $(this).attr("data-character");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + character + "api_key=c556iUpaeO6MaqfvC272iCg8GqrElGfo";
+
+    var character = $(this).attr("data-name");
    
+    var queryURL = "https//api.giphy.com/v1/gifs/search?api_key=c556iUpaeO6MaqfvC272iCg8GqrElGfo&q=" + character + "&limit=10&offset0&rating=G&lang=en";
+
+
     $("#buttons").on('click', function(){
         console.log("click")
         $.ajax({
@@ -24,7 +25,7 @@ $(document).ready(function(){
         console.log(response)
 
         for (var i = 0; i < results.length; i++) {
-            var gifAppear = $("<div class='item'>");
+            var gifAppear = $("#appear");
 
             var rating = results[i].rating;
 
@@ -32,11 +33,16 @@ $(document).ready(function(){
 
             var characterImage = $("<img>");
             characterImage.attr("src", results[i].images.fixed_height.url);
+            characterImag.attr("src", results[i].images.fixed_width_still.url);
+            characterImag.attr("data-still", results[i].images.fixed_width_still.url);
+            characterImag.attr("data-move", results[i].images.fixed_width.url);
+            characterImag.attr("data-state", "still");
 
             gifAppear.prepend(p);
             gifAppear.prepend(characterImage);
 
             $("#appear").append(gifAppear);
+            $("#appear").append(rating);
         }
 
       });

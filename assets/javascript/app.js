e736a7ff3e1b71f33dda4c19a60e21e1647d5408
ckgibb61 <giblinck@gmail.com>
2018-03-18 22:18:20 -0600

    Add files via upload

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,52 +1,55 @@
-$(document).ready(function(){
-    console.log("ready");
-  
-   var topics = ["Leslie Knope", "April Ludgate", "Ron Swanson", "Andy Dwyer"];
+var topics = ["Leslie Knope", "Andy Dwyer", "Ron Swanson", "Andy Dwyer"];
+
+$(document).ready(function () {
+ console.log("ready");
 
 
-     for (var i = 0; i < topics.length; i++) {
-        console.log(topics[i]);
-        var Btn = $("<button>").text(topics[i]);
-        $("#buttons").append(Btn);
-    };
-
-    var character = $(this).attr("data-name");
-   
-    var queryURL = "http//api.giphy.com/v1/gifs/search?api_key=c556iUpaeO6MaqfvC272iCg8GqrElGfo&q=" + character + "&limit=10&offset0&rating=G&lang=en";
+ for (var i = 0; i < topics.length; i++) {
+   console.log(topics[i]);
+   var Btn = $("<button>").text(topics[i]);
+   Btn.attr("id", "btn")
+//    Btn.attr("search", topics[i])
+   $("#buttons").append(Btn);
+ };
 
 
-    $("#buttons").on('click', function(){
-        console.log("click")
-        $.ajax({
-        url: queryURL,
-        method: "GET"
-      }).then(function(response) {
-        var results = response.data;
-        console.log(response)
+ $("#buttons").on("click", function () {
+   console.log("click")
+   var character = $(this).text();
+   var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=c556iUpaeO6MaqfvC272iCg8GqrElGfo&q=" + character + "&limit=25&offset=0&rating=G&lang=en";
+   $.ajax({
+     url: queryURL,
+     method: "GET"
+   }).then(function (response) {
+     var results = response.data;
+     console.log(response);
 
-        for (var i = 0; i < results.length; i++) {
-            var gifAppear = $("#appear");
+     for (var i = 0; i < results.length; i++) {
+       var gifAppear = $("#appear");
 
-            var rating = results[i].rating;
+       var rating = results[i].rating;
 
-            var p = $("<p>").text("Rating: " + rating);
+       var p = $("<p>").text("Rating: " + rating);
 
-            var characterImage = $("<img>");
-            characterImage.attr("src", results[i].images.fixed_height.url);
-            characterImag.attr("src", results[i].images.fixed_width_still.url);
-            characterImag.attr("data-still", results[i].images.fixed_width_still.url);
-            characterImag.attr("data-move", results[i].images.fixed_width.url);
-            characterImag.attr("data-state", "still");
+       var characterImage = $("<img>");
+       characterImage.attr("src", results[i].images.fixed_height.url);
 
-            gifAppear.prepend(p);
-            gifAppear.prepend(characterImage);
+       gifAppear.prepend(p);
+       gifAppear.prepend(characterImage);
 
-            $("#appear").append(gifAppear);
-            $("#appear").append(rating);
-        }
-
-      });
-    });
+       $("#appear").append(gifAppear);
+       $("#appear").append(rating);
+     };
+    
 
 
+    //  $("#appear").on("click", function () {
+    //     var gif = $(this).attr(src);
+    //     $gif.attr("src", imgAlt).attr("data-altImage", imgSrc);
+	// 	$gif.parent().toggleClass("play"
+       
+    //  });
+   });
+ });
+ 
 });
